04a9cb9b326892efe5e9077e56a6acb55e0624f9
Marius Kittler <mkittler@suse.de>
2019-01-14 13:20:36 +0100

    Outsource function to clone single repo from os-autoinst orga

---
+++ lib/clone_repo.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+
+# define function to clone repo and add fork
+function clone_repo() {
+    local repo_name=${1}
+    local repo_as=${2:-$repo_name}
+    local repo_url="https://github.com/os-autoinst/$repo_name"
+
+    if [[ -d $repo_name ]]; then
+        echo "Skipping $repo_name; already exists."
+        return 0
+    fi
+
+    git clone "$repo_url" "$repo_as"
+    pushd "$repo_as"
+    git remote add "$gh_user" "git@github.com:$gh_user/$repo_name.git"
+    git remote update
+    popd
+}
